c458a9c766a3588e911d3aa85449ab04ddc5a836
riquetaines <129297394+riquetaines@users.noreply.github.com>
2024-10-18 11:47:42 -0300

    controller.py

--- controller.py
+++ controller.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request # redirect, url_for
-from model import Pessoa, pessoas,add_pessoa
+from flask import Flask, render_template, request, make_response, redirect, url_for
+from model.model import Pessoa, pessoas,add_pessoa
 
 app = Flask(__name__)
 
@@ -17,3 +17,17 @@ def add():
     senha = request.form["pessoa"]
     add_pessoa(nome, senha)
     return render_template('form.html', pessoas=pessoas)
+
+@app.route('/set_cookie')
+    def set_cookie():
+        resp = make_response("cookie foi setado")
+        resp.set.cookie('cookie', 'atena', max_age=60*60*24)
+
+@app.route('/get_cookie')
+    def get_cookie():
+        cookie = request.coookies.get('cookie')
+        if cookie:
+            return f'o nome é {cookie}'
+        else:
+            return 'cookie não encontrado'
+        
